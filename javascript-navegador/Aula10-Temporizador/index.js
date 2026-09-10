e25c5d8fa4d1417177ea9b0402d4e08e03ad8239
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-10 20:50:46 -0300

    Temporizador

---
+++ javascript-navegador/Aula10-Temporizador/index.js
@@ -0,0 +1,13 @@
+// setInterval -> executa algo infinatemente em um determinado tempo
+
+// function acao() {
+//     document.write("Executando...")
+// }
+
+// setInterval(acao, 1000)
+
+// const timer = setInterval(() => {
+//     document.write("Executando... <br>")
+// }, 1000)
+
+// clearInterval(timer)
