91690cacdc17454b69bec9ed1ebc1aac50ab687f
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-10 13:14:21 -0300

    Comandos de entrada

---
+++ javascript-navegador/Aula01/index.js
@@ -0,0 +1,6 @@
+let nome = prompt("Qual o seu nome?");
+let sobrenome = prompt("Qual é o seu sobrenome?");
+
+console.log(`Olá ${nome} ${sobrenome}`);
+
+document.write(`<h1>Olá, Seja bem vindo ${nome} ${sobrenome}</h1>`);
