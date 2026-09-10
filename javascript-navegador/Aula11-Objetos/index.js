b29c34b95dce48135ff262c65648951116079e79
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-10 20:53:56 -0300

    objetos

---
+++ javascript-navegador/Aula11-Objetos/index.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: "Gabriel",
+    sobrenome: "Liz",
+    idade: 19,
+    altura: 1.96,
+    vivo: false,
+    profissao: "Programador"
+}
+
+console.log(pessoa)
+console.log(pessoa.nome)
+console.log(pessoa.vivo ? "Vivooo" : "morreuuuu")
