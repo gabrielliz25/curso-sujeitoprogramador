9a729357cfa5e46633258f7cddaa4c4ee7625dab
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-10 20:00:44 -0300

    condicionais

---
+++ javascript-navegador/Aula07-Condicionais/index.js
@@ -0,0 +1,30 @@
+// const n1 = 30
+// const n1 = 50
+// const n1 = "30"
+const n1 = 50
+
+if (n1 === 30) {
+    console.log("Numero igual a 30")
+} else {
+    console.log("Numero diferente de 30")
+}
+
+if (n1 >= 30) {
+    console.log("Número é maior ou igual a 30")
+} else {
+    console.log("Número é menor que 30")
+}
+
+// -------------------------------
+
+const n2 = 10;
+
+n2 === 10 ? console.log("Número igual a 10") : console.log("Número diferente de 10")
+
+let n3 = 2
+let n4 = 10
+
+let soma = n3 + n4
+let ehPar = soma%2 === 0 ? true : false
+
+console.log(ehPar)
