237fd95bb7841d3b07d69f378cc17f3b86b7fdff
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-10 14:24:05 -0300

    Switch

---
+++ javascript-navegador/Aula06-Switch/index.js
@@ -0,0 +1,26 @@
+function escolher() {
+    const val = prompt("Escolha de 1 a 4:")
+    console.log(typeof val)
+    switch (val) {
+        case "1":
+            renderElement("Você escolheu Água 🫗")
+            break    
+        case "2":
+            renderElement("Você escolheu Café ☕")
+            break
+        case "3":
+            renderElement("Você escolheu Nescau 🍵")
+            break
+        case "4":
+            renderElement("Você escolheu Chimarrão 🧉")
+            break
+        default:
+            renderElement("Escolha um valor adequado!")
+            break
+    }
+}
+
+function renderElement(text) {
+    const paragrafo = document.getElementById("paragrafo")
+    paragrafo.innerText = text
+}
