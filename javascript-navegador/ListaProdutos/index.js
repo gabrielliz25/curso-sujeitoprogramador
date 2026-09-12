06d269645b3d8ee5584cd5cd2cc29bb72d16990b
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-12 10:34:10 -0300

    lista produtos 2

--- javascript-navegador/ListaProdutos/index.js
+++ javascript-navegador/ListaProdutos/index.js
@@ -1,4 +1,4 @@
-const listProduts = ["Computado", "Telefone", "Mouse", "Teclado"]
+const listProduts = ["Computador", "Telefone", "Mouse", "Teclado"]
 console.log("Quantidade:", listProduts.length)
 
 // Remova o elemento "Mouse"
@@ -7,11 +7,17 @@ listProduts.splice(indexItem, 1)
 console.log(listProduts)
 
 // Procurando elemento "Computador"
-const busca = listProduts.find(el => {
-    if (el === "Computador") {
-        return "Computador Encontrado"
-    } else {
-        return "Computador não encontrado"
-    }
-})
-console.log(busca)
+const busca = listProduts.find(el => el === "Computador")
+console.log(busca ? "Computador encontrado" : "Computador não encontrado")
+
+console.log(" ")
+
+const listNumers = [1, 3, 5, 7, 0, 9]
+console.log("Lista ordenada:", listNumers.sort())
+
+// Remover o primeiro numero
+const numRemovido = listNumers.shift()
+console.log(listNumers, numRemovido)
+
+// Inverter ordem
+console.log("Lista inversa:", listNumers.sort().reverse())
