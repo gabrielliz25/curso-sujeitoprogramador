6ce6a6285babc40b5cbea1ab5dfc84e0d1632d80
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-14 13:57:33 -0300

    renderConditional 02

--- reactJS/src/App.jsx
+++ reactJS/src/App.jsx
@@ -5,14 +5,16 @@
 // import State from "./components/State";
 
 // Renderização Condicional
-import RenderConditional from "./components/RenderConditional";
+// import RenderConditional from "./components/RenderConditional";
+
+
 
 const App = () => {
     return (
         <>
             <h1>Projeto de ReactJS</h1>
 
-            <RenderConditional valor={"100"} />
+            
         </>
     );
 };
--- reactJS/src/components/RenderConditional.jsx
+++ reactJS/src/components/RenderConditional.jsx
@@ -1,7 +1,10 @@
-const RenderConditional = ({ valor }) => {
+const RenderConditional = ({ valor, user }) => {
     return <>
     {valor && <h2>Valor existente: {valor}</h2>}
     {valor === "100" ? <p>Valor 100</p> : <p>Qualquer outro valor</p>}
+    <p>
+        Olá {user === "Gabriel" ? "ADMIN" : "Visitante"}
+    </p>
     </>;
 };
 
