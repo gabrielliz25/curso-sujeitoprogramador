04f832a2eb1a801a680ca9f3893edf7d947a885c
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-14 20:20:11 -0300

    alteração app

--- reactJS/src/App.jsx
+++ reactJS/src/App.jsx
@@ -1,7 +1,7 @@
 // Aula de props
 // import Props from "./components/Props";
 
-import Requisicoes from "./components/Requisicoes";
+
 
 // useState
 // import State from "./components/State";
@@ -20,13 +20,14 @@ import Requisicoes from "./components/Requisicoes";
 // import Effect02 from "./components/Effect02";
 
 // Requisições
+// import Requisicoes from "./components/Requisicoes";
 
 const App = () => {
     return (
         <>
             <h1>Projeto de ReactJS</h1>
 
-            <Requisicoes />
+            
         </>
     );
 };
