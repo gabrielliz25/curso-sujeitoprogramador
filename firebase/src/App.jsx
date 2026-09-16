cafbf5ce0e50de5d9c2186ba8a9bc57ec95458c8
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-16 14:35:37 -0300

    buscando itens no banco

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -1,10 +1,12 @@
 import AddItem from "./components/AddItem";
+import BuscarItens from "./components/BuscarItens";
 
 const App = () => {
     return <>
         <div>
             <h1>React + Firebase</h1>
             <AddItem />
+            <BuscarItens />
         </div>
     </>
 };
---
+++ firebase/src/components/BuscarItens.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { db } from "../config/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+const BuscarItems = () => {
+    const [posts, setPosts] = useState([]);
+
+    const buscar = async () => {
+        const postRef = collection(db, "posts");
+
+        await getDocs(postRef)
+            .then((snapshot) => {
+                let lista = [];
+
+                snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        titulo: doc.data().titulo,
+                        autor: doc.data().autor,
+                    });
+                });
+
+                setPosts(lista);
+            })
+            .catch((err) => console.log("ERRO AO BUSCAR", err));
+    };
+
+    return (
+        <>
+            <button onClick={buscar}>Buscar</button>
+            <div>
+                {posts &&
+                    posts.map((post) => (
+                        <>
+                            <span>
+                                <h3>{post.titulo}</h3>
+                                <p>Por: {post.autor}</p>
+                            </span>
+                            <hr />
+                        </>
+                    ))}
+            </div>
+        </>
+    );
+};
+
+export default BuscarItems;
