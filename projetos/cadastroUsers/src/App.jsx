3c0ea4394a8368e1c2351e1ce28b78610a958da3
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-16 16:42:38 -0300

    cadastro de usuarios

--- projetos/cadastroUsers/src/App.jsx
+++ projetos/cadastroUsers/src/App.jsx
@@ -1,122 +1,141 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { db } from "./config/firebase";
+import {
+    collection,
+    addDoc,
+    onSnapshot,
+    doc,
+    deleteDoc,
+    updateDoc,
+} from "firebase/firestore";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [id, setId] = useState("");
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    useEffect(() => {
+        const unsub = onSnapshot(collection(db, "users"), (snapshot) => {
+            const lista = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setUsers(lista);
+        });
 
-      <div className="ticks"></div>
+        return () => unsub();
+    }, []);
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
+        if (!nome || !email) {
+            alert("Por favor, preencha todos os campos.");
+            return;
+        }
 
-export default App
+        setLoading(true);
+        await addDoc(collection(db, "users"), {
+            nome: nome,
+            email: email,
+        })
+            .then(() => {
+                console.log("Usuário cadastrado com sucesso!");
+                setNome("");
+                setEmail("");
+                setLoading(false);
+            })
+            .catch((err) => console.log("Erro:", err));
+    };
+
+    const handleDelete = async (id) => {
+        const docRef = doc(db, "users", id);
+        await deleteDoc(docRef)
+            .then(() => {
+                console.log("Item deletado com sucesso!");
+            })
+            .catch((err) => {
+                alert("Erro ao deletar documento!");
+                console.log(err);
+            });
+    };
+
+    const handleEdit = async (user) => {
+        setNome(user.nome);
+        setEmail(user.email);
+        setId(user.id);
+    };
+
+    const editSubmit = async (e) => {
+        e.preventDefault();
+        if (!nome || !email) {
+            alert("preencha os dados");
+            return;
+        }
+
+        const docRef = doc(db, "users", id);
+        setLoading(true)
+        await updateDoc(docRef, {
+            nome: nome,
+            email: email,
+        })
+        .then(() => {
+            console.log("Usuário atualizado com sucesso!");
+            setNome("");
+            setEmail("");
+            setId("");
+            setLoading(false);
+        })
+        .catch(err => console.log(err))
+    };
+
+    return (
+        <>
+            <div>
+                <form>
+                    <input type="hidden" value={id} />
+                    <input
+                        type="text"
+                        placeholder="Nome"
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                    />{" "}
+                    <br />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />{" "}
+                    <br />
+                    <button onClick={handleSubmit}>
+                        {loading ? "Cadastrando..." : "Cadastrar"}
+                    </button>
+                    <button onClick={editSubmit}>
+                        {loading ? "Atualizando..." : "Atualizar"}
+                    </button>
+                </form>
+
+                {users &&
+                    users.map((user) => (
+                        <>
+                            <p>
+                                {user.nome} - {user.email}{" "}
+                                <button onClick={() => handleDelete(user.id)}>
+                                    Excluir
+                                </button>
+                                <button onClick={() => handleEdit(user)}>
+                                    Editar
+                                </button>
+                            </p>
+                            <hr />
+                        </>
+                    ))}
+            </div>
+        </>
+    );
+};
+
+export default App;
