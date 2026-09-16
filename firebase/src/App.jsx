f8f6da644a3bb490428158d0eb284b8f146d6d57
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-16 14:24:23 -0300

    alteração no layout da pagina

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -1,63 +1,12 @@
-import { useState } from "react";
-import { db } from "./config/firebase";
-import { doc, setDoc, addDoc, collection, getDoc } from "firebase/firestore";
+import AddItem from "./components/AddItem";
 
 const App = () => {
-    const [titulo, setTitulo] = useState("");
-    const [autor, setAutor] = useState("");
-
-    const adicionar = async (evt) => {
-        evt.preventDefault();
-        // await setDoc(doc(db, "posts", "789"), {
-        //     titulo: titulo,
-        //     autor: autor,
-        // })
-        //     .then(() => {
-        //         console.log("Dados Cadastrados no Banco");
-        //     })
-        //     .catch((err) => console.log("ERRO:", err));
-
-        await addDoc(collection(db, "posts"), {
-            titulo: titulo,
-            autor: autor,
-        })
-            .then(() => console.log("Dados Cadastrados no Banco"))
-            .catch((err) => console.log("ERRO:", err));
-    };
-
-    async function buscar() {
-        const post = doc(db, "posts", "456");
-
-        await getDoc(post)
-            .then((snapshot) => {
-                setTitulo(snapshot.data().titulo);
-                setAutor(snapshot.data().autor);
-            })
-            .catch((err) => console.log("ERRO AO BUSCAR", err));
-    }
-
-    return (
-        <>
-            <form onSubmit={adicionar}>
-                <input
-                    type="text"
-                    placeholder="Digite o titulo"
-                    onChange={(evt) => setTitulo(evt.target.value)}
-                    value={titulo}
-                />
-                <br />
-                <input
-                    type="text"
-                    placeholder="Digite o autor"
-                    onChange={(evt) => setAutor(evt.target.value)}
-                    value={autor}
-                />
-                <br />
-                <button type="submit">Cadastrar</button>
-                <button onClick={buscar}>Buscar Item</button>
-            </form>
-        </>
-    );
+    return <>
+        <div>
+            <h1>React + Firebase</h1>
+            <AddItem />
+        </div>
+    </>
 };
 
 export default App;
---
+++ firebase/src/components/AddItem.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { db } from "../config/firebase";
+import { addDoc, collection, doc, getDoc } from "firebase/firestore";
+
+const AddItem = () => {
+    const [titulo, setTitulo] = useState("");
+    const [autor, setAutor] = useState("");
+
+    const adicionar = async (evt) => {
+        evt.preventDefault();
+        // await setDoc(doc(db, "posts", "789"), {
+        //     titulo: titulo,
+        //     autor: autor,
+        // })
+        //     .then(() => {
+        //         console.log("Dados Cadastrados no Banco");
+        //     })
+        //     .catch((err) => console.log("ERRO:", err));
+
+        if (titulo === "" || autor === "") {
+            alert("Preencha todos os campos");
+            return;
+        }
+
+        await addDoc(collection(db, "posts"), {
+            titulo: titulo,
+            autor: autor,
+        })
+            .then(() => {
+                setAutor("");
+                setTitulo("");
+                alert("Dados Cadastrados no Banco");
+            })
+            .catch((err) => console.log("ERRO:", err));
+    };
+
+    async function buscar(evt) {
+        evt.preventDefault();
+        const post = doc(db, "posts", "LmRfWXjv2C75OsuDOj0M");
+
+        await getDoc(post)
+            .then((snapshot) => {
+                setTitulo(snapshot.data().titulo);
+                setAutor(snapshot.data().autor);
+            })
+            .catch((err) => console.log("ERRO AO BUSCAR", err));
+    }
+
+    return (
+        <>
+            <form>
+                <input
+                    type="text"
+                    placeholder="Digite o titulo"
+                    onChange={(evt) => setTitulo(evt.target.value)}
+                    value={titulo}
+                />
+                <br />
+                <input
+                    type="text"
+                    placeholder="Digite o autor"
+                    onChange={(evt) => setAutor(evt.target.value)}
+                    value={autor}
+                />
+                <br />
+                <button onClick={adicionar}>Cadastrar</button>
+                <button onClick={buscar}>Buscar Item</button>
+            </form>
+        </>
+    );
+};
+
+export default AddItem;
