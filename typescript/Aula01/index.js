2e7cd51822bf2857a2934935f4c81089376d1e11
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-17 13:19:24 -0300

    primeira parte typescript

---
+++ typescript/Aula01/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const nome = "Gabriel";
+console.log("Bem vindo", nome);
---
+++ typescript/Aula01/index.ts
@@ -0,0 +1,10 @@
+const nome = "Gabriel";
+console.log("Bem vindo", nome);
+
+/*
+Desenvolvemos em typescript, depois compilamos para javascript e rodamos no navegador o javascript
+
+- tsc index.ts
+*/
+
+
