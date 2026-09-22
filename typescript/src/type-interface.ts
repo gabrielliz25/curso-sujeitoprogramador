cc63bd08c91113eea1010cc12ca14db3165c7568
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-22 13:10:24 -0300

    type e interfaces

---
+++ typescript/src/type-interface.ts
@@ -0,0 +1,32 @@
+interface Post {
+    nome: string
+}
+
+interface Conteudo {
+    descricao: string
+}
+
+type Post2 = {
+    nome: string
+}
+
+type Conteudo2 = {
+    descricao: string
+}
+
+interface Blog extends Post, Conteudo {
+    nome: string,
+    descricao: string
+}
+
+type Blog2 = Post & Conteudo
+
+interface Soma {
+    (v1: number, v2: number): number
+}
+
+type Soma2 = (v1: number, v2: number) => number
+
+// interface UserId extends string | number {} Não tem como fazer isso
+type UserId = string | number
+
